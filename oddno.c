73a43533a1f86d253613b20226d3bc2a243d1a76
afreethaanwar <30343460+afreethaanwar@users.noreply.github.com>
2017-08-12 16:48:25 +0530

    Create oddno.c

---
+++ oddno.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int number,m=10,n=20;
+printf("print the odd nos in the given range");
+for(number=m;number<=n;number++)
+{
+if(number%2==0)
+printf("%d",number);
+}
+getch();
+}
