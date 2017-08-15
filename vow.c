8755aed8a324a6a161a5d82bad9f430fb44be562
afreethaanwar <30343460+afreethaanwar@users.noreply.github.com>
2017-08-15 19:29:15 +0530

    Rename vowel.c to vow.c

---
+++ vow.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int countDigits,countAlphabet,countSpecialChar;
+    int counter;
+    countDigits=countAlphabet=countSpecialChar=0;
+     printf("Enter a string: ");
+    gets(str);
+    for(counter=0;str[counter]!=NULL;counter++)
+    {
+ 
+        if(str[counter]>='0' && str[counter]<='9')
+            countDigits++;
+        else if((str[counter]>='A' && str[counter]<='Z')||(str[counter]>='a' && str[counter]<='z'))
+            countAlphabet++;
+        else if(str[counter]==' ')
+            countSpecialChar++;
+    }
+    printf("\nDigits: %d \nAlphabets: %d \nSpecial Characters: %d",countDigits,countAlphabet,countSpecialChar);
+    return 0;
+}
