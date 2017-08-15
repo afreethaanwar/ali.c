399332c50b2690ddfef07818b5f04057831ca757
afreethaanwar <30343460+afreethaanwar@users.noreply.github.com>
2017-08-15 10:34:00 +0530

    Create Co.c

---
+++ Co.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any alphabet: ");
+    scanf("%c", &ch);
+    switch(ch)
+    {
+        case 'a': printf("VOWEL");
+            break;
+        case 'e': printf("VOWEL");
+            break;
+        case 'i': printf("VOWEL");
+            break;
+        case 'o': printf("VOWEL");
+            break;
+        case 'u': printf("VOWEL");
+            break;
+        case 'A': printf("VOWEL");
+            break;
+        case 'E': printf("VOWEL");
+            break;
+        case 'I': printf("VOWEL");
+            break;
+        case 'O': printf("VOWEL");
+            break;
+        case 'U': printf("VOWEL");
+            break;
+        default: printf("CONSONANT");
+    }
+
+    return 0;
+}
